80dd81d368fc40e04025f978003ef7aec6cc2821
angelgamaza <angel.gamaza@gmail.com>
2020-03-21 13:21:15 +0100

    Solved warnings with Spring Data Repositories explicitly defining base packages for MySQL and MongoDB

--- src/main/java/com/gamaza/rest4cep/Rest4CepApplication.java
+++ src/main/java/com/gamaza/rest4cep/Rest4CepApplication.java
@@ -3,9 +3,16 @@ package com.gamaza.rest4cep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {
+        "com.gamaza.rest4cep.mysql.dao",
+        "com.gamaza.rest4cep.config.security.dao"
+})
+@EnableMongoRepositories(basePackages = "com.gamaza.rest4cep.mongo.dao")
 @EnableJpaAuditing
 @EnableMongoAuditing
 public class Rest4CepApplication {
